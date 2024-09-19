ef0eb6b19b66659b468e84f54915adee919cbb04
Rijal Ghodi <rijalghodi10@gmail.com>
2024-09-19 14:08:44 +0700

    set cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const allowedOrigins = (process.env.CORS_ORIGINS || '').split(',');
+
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: process.env.CORS_METHODS || 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: process.env.CORS_CREDENTIALS === 'true',
+  });
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
