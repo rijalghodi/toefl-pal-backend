28cc089187ae8dfebf723c49e0476d2cfb08e938
Rijal Ghodi <rijalghodi10@gmail.com>
2024-09-24 13:23:49 +0700

    wip: add relation to question and option in key

--- src/key/key.service.ts
+++ src/key/key.service.ts
@@ -69,6 +69,7 @@ export class KeyService {
   async findOne(id: string): Promise<Key> {
     const key = await this.keyRepo.findOne({
       where: { id },
+      relations: ['option', 'question']
     });
     if (!key) throw new NotFoundException(`Key with id ${id} not found`);
     return key;
@@ -76,6 +77,7 @@ export class KeyService {
   async findOneByQuestionId(questionId: string): Promise<Key> {
     const key = await this.keyRepo.findOne({
       where: { question: { id: questionId } },
+      relations: ['option', 'question'] 
     });
     if (!key)
       throw new NotFoundException(
